ec8d1f03127d930476868f318b0635818d3ab71b
vanesascode <vanesa.juarez.paris@gmail.com>
2023-06-14 13:26:11 +0200

    Pagination added

--- rick-and-morty-app/src/App.js
+++ rick-and-morty-app/src/App.js
@@ -8,15 +8,28 @@ import Pagination from './components/Pagination'
 function App() {
 
   const [characters, setcharacters] = useState([])
+  const [info, setinfo] = useState({})
 
   const initialUrl = 'https://rickandmortyapi.com/api/character'
 
   const fetchCharacters = (url) => {
     fetch(url)
       .then(response => response.json())
-      .then(data => setcharacters(data.results))
+      .then((data) => {
+        setcharacters(data.results);
+        setinfo(data.info);
+      })
       .catch(error => console.log(error))
+  };
+
+  const onPrevious = () => {
+    fetchCharacters(info.next);
   }
+
+  const onNext = () => {
+    fetchCharacters(info.next);
+  }
+
 
   useEffect(() => {
     fetchCharacters(initialUrl);
@@ -29,9 +42,9 @@ function App() {
 
       <Navbar brand='Characters' />
       <div className='container mt-4'>
-        <Pagination />
+        <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext} />
         <Characters characters={characters} />
-        <Pagination />
+        <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext} />
       </div>
 
     </>
--- rick-and-morty-app/src/components/Pagination.js
+++ rick-and-morty-app/src/components/Pagination.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 
-const Pagination = ({ onPrevious, onNext }) => {
+const Pagination = ({ prev, next, onPrevious, onNext }) => {
 
   const handlePrevious = () => {
     onPrevious();
@@ -16,14 +16,22 @@ const Pagination = ({ onPrevious, onNext }) => {
   return (
     <nav>
       <ul className="pagination justify-content-end">
-        <li className="page-item">
-          <button className="mt-0 page-link link-dark" onClick={handlePrevious}>Previous</button>
-        </li>
-        <li className="page-item">
-          <button className="mt-0 page-link link-dark" onClick={handleNext}>More Characters</button>
-        </li>
+        {
+          prev ?
+            (<li className="page-item">
+              <button className="mt-0 page-link link-dark" onClick={handlePrevious}>Previous</button>
+            </li>)
+            :
+            null
+        }
+        {
+          next ? (<li className="page-item">
+            <button className="mt-0 page-link link-dark" onClick={handleNext}>More Characters</button>
+          </li>)
+            :
+            null
+        }
       </ul>
-
     </nav>
   )
 }
